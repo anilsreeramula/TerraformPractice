061248d352ada6497d7c72c17a6082d46e08567b
root <root@ip-172-31-10-154.ca-central-1.compute.internal>
2026-06-03 14:01:24 +0000

    changes in main.tf -day04

--- Day04-Statefile-remote_backend/main.tf
+++ Day04-Statefile-remote_backend/main.tf
@@ -9,3 +9,25 @@ resource "aws_instance" "name" {
     }
     
 }
+
+resource "aws_vpc" "vpc" {
+    cidr_block = "192.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet" {
+    vpc_id = aws_vpc.vpc.id
+    cidr_block = "192.0.0.0/24"
+}
+
+
+resource "aws_instance" "name2" {
+    ami           = "ami-06445ac85e0d277a9"
+    instance_type = "t3.small" 
+        tags = {
+         Name = "app_server_02"
+    }
+    
+}
+
+
+

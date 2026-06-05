23c8940919df22e5561be647eadda971ce39cd2c
root <root@ip-172-31-29-227.ca-central-1.compute.internal>
2026-06-05 10:48:24 +0000

    user2 updates

--- Day05-remote-backend-statelock/main.tf
+++ Day05-remote-backend-statelock/main.tf
@@ -4,8 +4,17 @@ resource "aws_vpc" "my_vpc" {
         Name = "my_vpc"
     }
 
+} 
+
+resource "aws_subnet" "my_subnet02" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.2.0/24"
+    tags = {
+        Name = "my_subnet2"
+    }
 }
 
+<<<<<<< Updated upstream
 resource "aws_subnet" "my_subnet01" {
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "10.0.1.0/24"
@@ -13,4 +22,8 @@ resource "aws_subnet" "my_subnet01" {
         Name = "my_subnet1"
     }
 }
+=======
+
+
+>>>>>>> Stashed changes
 
